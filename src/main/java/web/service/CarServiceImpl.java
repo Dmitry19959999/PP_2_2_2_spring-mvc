eaf550e21fb0dc44abac568a4e59ccb9b6083d3e
dmitr <dimanmosenko@gmail.com>
2023-05-15 22:06:40 +0300

    add

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,8 @@
 package web.service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
-
 import java.util.List;
 
 @Service
